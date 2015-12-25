5938a458c2c85ff23cd1aff87c0db1ca8751fb01
Delyan Raychev <delyanr@gmail.com>
2015-12-24 16:58:47 -0800

    Use perl to parse the APRS message

--- parse_aprs_packet.pl
+++ parse_aprs_packet.pl
@@ -1,18 +1,20 @@
 #!/usr/bin/perl
+#!/usr/bin/env perl
 
 use Ham::APRS::FAP qw(parseaprs);
 #my $aprspacket = 'OH2RDP>BEACON,OH2RDG*,WIDE:!6028.51N/02505.68E#PHG7220/RELAY,WIDE, OH2AP Jarvenpaa';
 
 my $aprspacket = $ARGV[0];
+
 my %packetdata;
 my $retval = parseaprs($aprspacket, \%packetdata);
 if ($retval == 1) {
-        # decoding ok, do something with the data
-	print "\{";
-	while (my ($key, $value) = each(%packetdata)) {
-		print "\"$key\": \"$value\",";
-	}
+    # decoding ok, do something with the data
+    print "\{";
+    while (my ($key, $value) = each(%packetdata)) {
+        print "\"$key\": \"$value\",";
+    }
 } else {
-	warn "\"Parsing failed\": \"$packetdata{resultmsg} ($packetdata{resultcode})\"";
+    warn "\"Parsing failed\": \"$packetdata{resultmsg} ($packetdata{resultcode})\"";
 }
 print "\}";
--- run_service.py
+++ run_service.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 from aprsclient import pull
+from subprocess import call
 
 if __name__ == '__main__':
     puller = pull.Pull('b/N0*')
     while True:
-        print puller.get_a_line()
+        line = puller.get_a_line()
+        call(["parse_aprs_packet.pl", line])
